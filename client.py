cb016d26be1c5ed05c83b2b3c609d6312b2b9acc
Megh Parikh <meghprkh@gmail.com>
2017-04-10 23:11:23 +0530

    Properly indent comments

--- client.py
+++ client.py
@@ -13,72 +13,72 @@ if len(sys.argv) < 3:
 
 timeout = float(sys.argv[4]) if len(sys.argv) > 4 else 0.01
 
-#takes the port number as command line arguments
+# takes the port number as command line arguments
 serverName="127.0.0.1"
 serverPort = int(sys.argv[1])
 
-#takes the file name as command line arguments
+# takes the file name as command line arguments
 filename = ''.join(sys.argv[2])
 
-#create client socket
+# create client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSocket.settimeout(timeout)
 
-#initializes window variables (upper and lower window bounds, position of next seq number)
+# initializes window variables (upper and lower window bounds, position of next seq number)
 base=1
 nextSeqnum=1
 windowSize = int(sys.argv[3]) if len(sys.argv) > 3 else 7
 window = []
 
-#SENDS DATA
+# SENDS DATA
 fileOpen= open(filename, 'rb')
 data = fileOpen.read(500)
 done = False
 lastackreceived = time.time()
 
 while not done or window:
-#	check if the window is full	or EOF has reached
+    # check if the window is full	or EOF has reached
 	if(nextSeqnum<base+windowSize) and not done:
-#		create packet(seqnum,data,checksum)
+        # create packet(seqnum,data,checksum)
 		sndpkt = []
 		sndpkt.append(nextSeqnum)
 		sndpkt.append(data)
 		h = hashlib.md5()
 		h.update(pickle.dumps(sndpkt))
 		sndpkt.append(h.digest())
-#		send packet
+        # send packet
 		transmit(clientSocket, pickle.dumps(sndpkt), serverName, serverPort)
 		print "Sent data", nextSeqnum
-#		increment variable nextSeqnum
+        # increment variable nextSeqnum
 		nextSeqnum = nextSeqnum + 1
-#		check if EOF has reached
+        # check if EOF has reached
 		if(not data):
 			done = True
-#		append packet to window
+        # append packet to window
 		window.append(sndpkt)
-#		read more data
+        # read more data
 		data = fileOpen.read(500)
 
-#RECEIPT OF AN ACK
+    # RECEIPT OF AN ACK
 	try:
 		packet,serverAddress = clientSocket.recvfrom(4096)
 		rcvpkt = []
 		rcvpkt = pickle.loads(packet)
-#		check value of checksum received (c) against checksum calculated (h)
+        # check value of checksum received (c) against checksum calculated (h)
 		c = rcvpkt[-1]
 		del rcvpkt[-1]
 		h = hashlib.md5()
 		h.update(pickle.dumps(rcvpkt))
 		if c == h.digest():
 			print "Received ack for", rcvpkt[0]
-#			slide window and reset timer
+            # slide window and reset timer
 			while rcvpkt[0]>base and window:
 				lastackreceived = time.time()
 				del window[0]
 				base = base + 1
 		else:
 			print "error detected"
-#TIMEOUT
+    # TIMEOUT
 	except:
 		if time.time() - lastackreceived > timeout:
 			for i in window:

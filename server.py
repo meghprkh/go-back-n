59fe3e52c49fe3176e4e6597fe68813a5f04a256
Megh Parikh <meghprkh@gmail.com>
2017-04-10 23:29:33 +0530

    Beautify code

--- server.py
+++ server.py
@@ -14,16 +14,16 @@ if len(sys.argv) < 2:
 # takes the port number as command line arguments and create server socket
 serverIP = "127.0.0.1"
 serverPort = int(sys.argv[1])
-timeout =  float(sys.argv[2]) if len(sys.argv) > 2 else 3
+timeout = float(sys.argv[2]) if len(sys.argv) > 2 else 3
 
-serverSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-serverSocket.bind((serverIP,serverPort))
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind((serverIP, serverPort))
 serverSocket.settimeout(timeout)
 print "Ready to serve"
 
 # initializes packet variables
-expectedseqnum=1
-ACK=1
+expectedseqnum = 1
+ACK = 1
 ack = []
 
 # RECEIVES DATA
@@ -34,53 +34,58 @@ starttime = time.time()
 
 while True:
 
-	try:
-		rcvpkt=[]
-		packet,clientAddress= serverSocket.recvfrom(4096)
-		rcvpkt = pickle.loads(packet)
-        # check value of checksum received (c) against checksum calculated (h) - NOT CORRUPT
-		c = rcvpkt[-1]
-		del rcvpkt[-1]
-		h = hashlib.md5()
-		h.update(pickle.dumps(rcvpkt))
-		if c == h.digest():
-            # check value of expected seq number against seq number received - IN ORDER
-			if(rcvpkt[0]==expectedseqnum):
-				print "Received inorder", expectedseqnum
-				if rcvpkt[1]:
-					f.write(rcvpkt[1])
-				else:
-					endoffile = True
-				expectedseqnum = expectedseqnum + 1
+    try:
+        rcvpkt = []
+        packet, clientAddress = serverSocket.recvfrom(4096)
+        rcvpkt = pickle.loads(packet)
+        # check value of checksum received (c) against checksum calculated (h) -
+        # NOT CORRUPT
+        c = rcvpkt[-1]
+        del rcvpkt[-1]
+        h = hashlib.md5()
+        h.update(pickle.dumps(rcvpkt))
+        if c == h.digest():
+            # check value of expected seq number against seq number received -
+            # IN ORDER
+            if(rcvpkt[0] == expectedseqnum):
+                print "Received inorder", expectedseqnum
+                if rcvpkt[1]:
+                    f.write(rcvpkt[1])
+                else:
+                    endoffile = True
+                expectedseqnum = expectedseqnum + 1
                 # create ACK (seqnum,checksum)
-				sndpkt = []
-				sndpkt.append(expectedseqnum)
-				h = hashlib.md5()
-				h.update(pickle.dumps(sndpkt))
-				sndpkt.append(h.digest())
-				transmit(serverSocket, pickle.dumps(sndpkt), clientAddress[0], clientAddress[1])
-				print "New Ack", expectedseqnum
+                sndpkt = []
+                sndpkt.append(expectedseqnum)
+                h = hashlib.md5()
+                h.update(pickle.dumps(sndpkt))
+                sndpkt.append(h.digest())
+                transmit(serverSocket, pickle.dumps(sndpkt),
+                         clientAddress[0], clientAddress[1])
+                print "New Ack", expectedseqnum
 
-			else:
-                # default? discard packet and resend ACK for most recently received inorder pkt
-				print "Received out of order", rcvpkt[0]
-				sndpkt = []
-				sndpkt.append(expectedseqnum)
-				h = hashlib.md5()
-				h.update(pickle.dumps(sndpkt))
-				sndpkt.append(h.digest())
-				transmit(serverSocket, pickle.dumps(sndpkt), clientAddress[0], clientAddress[1])
-				print "Ack", expectedseqnum
-		else:
-			print "error detected"
-	except:
-		if endoffile:
-			if time.time() - lastpktreceived > timeout:
-				break
+            else:
+                # default? discard packet and resend ACK for most recently
+                # received inorder pkt
+                print "Received out of order", rcvpkt[0]
+                sndpkt = []
+                sndpkt.append(expectedseqnum)
+                h = hashlib.md5()
+                h.update(pickle.dumps(sndpkt))
+                sndpkt.append(h.digest())
+                transmit(serverSocket, pickle.dumps(sndpkt),
+                         clientAddress[0], clientAddress[1])
+                print "Ack", expectedseqnum
+        else:
+            print "error detected"
+    except:
+        if endoffile:
+            if time.time() - lastpktreceived > timeout:
+                break
 
 
 endtime = time.time()
 
 f.close()
 print 'FILE TRANFER SUCCESSFUL'
-print "TIME TAKEN " , str(endtime - starttime)
+print "TIME TAKEN ", str(endtime - starttime)
--- transmit.py
+++ transmit.py
@@ -2,65 +2,62 @@
 
 from random import randint
 
-dummy=' '
-reorder=0
+dummy = ' '
+reorder = 0
 counter = 1
-error=1
+error = 1
 p = 5
 
-def transmit(csocket,message,serverName,serverPort):
+
+def transmit(csocket, message, serverName, serverPort):
     global counter, error, reorder, dummy, p
     if (randint(1, p) % p) != 0:
-    	csocket.sendto(message,(serverName,serverPort))
-    	print 'Sends properly packet No ' + str(counter)
+        csocket.sendto(message, (serverName, serverPort))
+        print 'Sends properly packet No ' + str(counter)
 
     else:
         # Make some error with probability p
-    	if error==1:
-    		print 'Dropped a packet No ' + str(counter)
-    		error=2
+        if error == 1:
+            print 'Dropped a packet No ' + str(counter)
+            error = 2
 
+        elif error == 2:
+            if reorder == 1:
+                print 're-ordering a packet No ' + str(counter)
+                csocket.sendto(message, (serverName, serverPort))
+                csocket.sendto(dummy, (serverName, serverPort))
+                reorder = 0
+                counter = counter + 1
+                error = 3
+            else:
+                dummy = message
+                reorder = 1
+                counter = counter - 1
 
-    	elif error==2:
-    		if reorder == 1:
-    			print 're-ordering a packet No ' + str(counter)
-    			csocket.sendto(message,(serverName,serverPort))
-    			csocket.sendto(dummy,(serverName,serverPort))
-    			reorder=0
-    			counter=counter+1
-    			error=3
-    		else:
-    			dummy=message
-    			reorder=1
-    			counter=counter-1
+        elif error == 3:
+            print 'Duplicated a packet No ' + str(counter)
+            csocket.sendto(message, (serverName, serverPort))
 
+            csocket.sendto(message, (serverName, serverPort))
+            error = 4
 
+        elif error == 4:
+            print 'creating packet errors packet No ' + str(counter)
 
-    	elif error==3:
-    		print 'Duplicated a packet No ' + str(counter)
-    		csocket.sendto(message,(serverName,serverPort))
+            mylist = list(message)
+        # get last char of the string
+            x = ord(mylist[-1])
+            if (x & 1) == 1:
+                # if first bit set, unset it
+                x &= ~(1)
+            else:
+                # if first bit not set, set it
+                x |= 1
 
-    		csocket.sendto(message,(serverName,serverPort))
-    		error=4
+            mylist[-1] = chr(x)
+            dummy = ''.join(mylist)
 
+            csocket.sendto(dummy, (serverName, serverPort))
+            error = 1
 
-    	elif error==4:
-    		print 'creating packet errors packet No ' + str(counter)
-
-    		mylist=list(message)
-            # get last char of the string
-    		x=ord(mylist[-1])
-    		if (x&1)==1:
-    			# if first bit set, unset it
-    			x &= ~(1)
-    		else:
-    			# if first bit not set, set it
-    			x |=  1
-
-    		mylist[-1]=chr(x)
-    		dummy=''.join(mylist)
-
-    		csocket.sendto(dummy,(serverName,serverPort))
-    		error=1
-
-    counter=counter+1
+    counter = counter + 1
